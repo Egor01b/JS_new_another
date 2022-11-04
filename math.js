86d7d0ba2c37856854b628445dae933a0645eca8
Egor Belous <belousegor5@gmail.com>
2022-11-04 12:47:47 +0200

    math operations js

---
+++ math.js
@@ -0,0 +1,10 @@
+/* var nUM1 = +prompt("Enter first number");
+var nUM2 = +prompt ("Enter second number");
+
+var ansWER = nUM1 + nUM2;
+var ansWER1 = nUM1 - nUM2;
+var ansWER2 = nUM1 * nUM2;
+var ansWER3 = nUM1 / nUM2;
+
+alert(`${nUM1}+${nUM2}=${ansWER},  ${nUM1}-${nUM2}=${ansWER1},  ${nUM1}*${nUM2}=${ansWER2},  ${nUM1}/${nUM2}=${ansWER3}`);
+console.log(ansWER,ansWER1,ansWER2,ansWER3); */
