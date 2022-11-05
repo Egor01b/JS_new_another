2f29a638d2367e077babd4671e3036d62ac8a9d3
Egor Belous <belousegor5@gmail.com>
2022-11-05 11:57:10 +0200

    add average number calc

---
+++ script.js
@@ -0,0 +1,9 @@
+// let firstNum = parseInt(prompt("Type first number"));
+// let secondNum = parseInt(prompt("Type second number"));
+// let thirdNum =parseInt(prompt("Type third number"));
+
+// let aveRageNum = Math.round((firstNum + secondNum + thirdNum)) / 3;
+
+// alert(`${aveRageNum} (average number)`);
+// console.log(aveRageNum);
+
